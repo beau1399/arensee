a7aada82785bbfd113527cc1d23fbe1d073c835e
dell <beau.wilkinson@gmail.com>
2022-02-01 21:54:33 -0500

    Naming

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ const noInterveningPiece = function(x,y,toX,toY) {
     const signX=Math.sign(toX-x);
     const signY=Math.sign(toY-y);
     for(let i=1; i<Math.max(Math.abs(toX-x),Math.abs(toY-y)); i+=1){
-	pieces.forEach((pt,pi)=>{ if(pt.x==x+signX*i && pt.y==y+signY*i && !pt.dead){returnable=false;}   });
+	pieces.forEach((pt,pi)=>{ if(pt.x==x+signX*i && pt.y==y+signY*i && !pt.deadness){returnable=false;}   });
     }
     return returnable;
 };
@@ -51,13 +51,13 @@ const releaseServer = (e,t) => {
 	    left = (targetX==2);
 	    if(left){
 		let castle=pieces.filter(u=>u.blackness==piecesn.blackness && u.y==piecesn.y && u.x==0)[0]
-		if(!castle.dirtiness && !piecesn.dirtiness && !castle.dead && !piecesn.dead) {
+		if(!castle.dirtiness && !piecesn.dirtiness && !castle.deadness && !piecesn.deadness) {
 		    if(!t.props.causesCheck(n,piecesn.x-1,targetY) && !t.props.causesCheck(n,piecesn.x-2,targetY)){
 			t.props.movePiece(n,piecesn.x-2,targetY);
 			t.props.movePiece(castle.n,castle.x+3,castle.y); }}
 	    }else{
 		let castle=pieces.filter(u=>u.blackness==piecesn.blackness && u.y==piecesn.y && u.x==7)[0]
-		if(!castle.dirtiness && !piecesn.dirtiness && !castle.dead && !piecesn.dead) {
+		if(!castle.dirtiness && !piecesn.dirtiness && !castle.deadness && !piecesn.deadness) {
 		    if(!t.props.causesCheck(n,piecesn.x+1,targetY) && !t.props.causesCheck(n,piecesn.x+2,targetY)){
 			t.props.movePiece(n,piecesn.x+1,targetY);
 			t.props.movePiece(n,piecesn.x+2,targetY);
@@ -73,7 +73,7 @@ class Piece extends Component {
     constructor(props){super(props);}
     
     render(){
-	if(this.props.dead) return null;
+	if(this.props.deadness) return null;
 	return (
 	    <Draggable shouldReverse={true /*We'll handle the positioning*/ }
 	    renderSize={92 } x={ this.props.x * 42 + 9} y={this.props.y * 42 + 138} onDragRelease={(event)=>{releaseServer(event,this)}}>
@@ -89,22 +89,22 @@ class Piece extends Component {
 const bishopCanMove = (blackness,x,y,toX,toY)=> (Math.abs(toX-x)==Math.abs(toY-y))
 					   && (!(x==toX && y==toY)) //It is helpful for game logic to exclude the identity TODO refactor
 					   && !pieces.some((t)=>
-					       t.x==toX && t.y==toY && t.blackness==blackness && !t.dead) //Can't move atop same color piece
+					       t.x==toX && t.y==toY && t.blackness==blackness && !t.deadness) //Can't move atop same color piece
 					   && noInterveningPiece(x,y,toX,toY)
 
 const rookCanMove = (blackness,x,y,toX,toY)=> (x==toX || y==toY)
 					 && (!(x==toX && y==toY)) //It is helpful for game logic to exclude the identity TODO refactor
 					 && !pieces.some((t)=>
-					     t.x==toX && t.y==toY && t.blackness==blackness && !t.dead) //Can't move atop same color piece
+					     t.x==toX && t.y==toY && t.blackness==blackness && !t.deadness) //Can't move atop same color piece
 					 && noInterveningPiece(x,y,toX,toY)
 
 function isChecked(blackness, setDbg, dbgString) {
     //King of the color that might be checked. We assume this exists.
-    const k=pieces.filter((t,i)=>t.blackness==blackness && t.kingness && !t.dead)[0];
+    const k=pieces.filter((t,i)=>t.blackness==blackness && t.kingness && !t.deadness)[0];
     let returnable = false;
     if(k) {
-	//TODO abstract the dead thing away
-	returnable= pieces.filter((t)=>!t.dead).some((t,i)=>t.blackness!=blackness && t.canMove(t.blackness,t.x,t.y,k.x,k.y));
+	//TODO abstract the deadness thing away
+	returnable= pieces.filter((t)=>!t.deadness).some((t,i)=>t.blackness!=blackness && t.canMove(t.blackness,t.x,t.y,k.x,k.y));
     }
     setDbg && setDbg(dbgString + '/' + (blackness?'BLACK':'WHITE')+' '+ (returnable?'IS':'IS NOT') + ' CHECKED')
     return returnable;
@@ -113,13 +113,13 @@ function isChecked(blackness, setDbg, dbgString) {
 const kingCanMove = (blackness,x,y,toX,toY)=> {
     return (Math.abs(toX-x)<=1 && Math.abs(toY-y)<=1
 	 && (!(x==toX && y==toY)) //It is helpful for game logic to exclude the identity TODO refactor	
-	 && !pieces.some((t)=>t.x==toX && t.y==toY && t.blackness==blackness && !t.dead) //TODO refactor
+	 && !pieces.some((t)=>t.x==toX && t.y==toY && t.blackness==blackness && !t.deadness) //TODO refactor
 )}
 
 const knightCanMove = (blackness,x,y,toX,toY)=> {
     return (Math.abs(toX-x) + Math.abs(toY-y)==3)
 	&& toX!=x && toY!=y
-	&& !pieces.some((t)=>t.x==toX && t.y==toY && t.blackness==blackness && !t.dead)
+	&& !pieces.some((t)=>t.x==toX && t.y==toY && t.blackness==blackness && !t.deadness)
 }
 
 
@@ -129,10 +129,10 @@ const enPassant = (blackness,pawnness,x,y,toX,toY)=> {
     const happened = pawnness && (
 	//Black
 	(toY-y==1 && blackness && Math.abs(toX-x)==1 &&
-	 pieces.some((t)=>t.x==toX && t.justAdvancedTwo && t.y==y && t.pawnness  && t.blackness!=blackness && !t.dead))||
+	 pieces.some((t)=>t.x==toX && t.justAdvancedTwo && t.y==y && t.pawnness  && t.blackness!=blackness && !t.deadness))||
 	//White
 	(toY-y==-1 && !blackness && Math.abs(toX-x)==1 &&
-	 pieces.some((t)=>t.x==toX && t.justAdvancedTwo && t.y==y && t.pawnness  && t.blackness!=blackness && !t.dead)));
+	 pieces.some((t)=>t.x==toX && t.justAdvancedTwo && t.y==y && t.pawnness  && t.blackness!=blackness && !t.deadness)));
 
     return {enpassant:happened, capturedX:toX, capturedY:y}
 }
@@ -153,11 +153,11 @@ const pawnCanMove = (blackness,x,y,toX,toY)=> {
 	  //Capture (pawn)
 	  // Customary diagonal capture, black
 	  ||(toY-y==1 && blackness && Math.abs(toX-x)==1 &&
-	     pieces.some((t)=>t.x==toX && t.y==toY && t.blackness!=blackness && !t.dead))
+	     pieces.some((t)=>t.x==toX && t.y==toY && t.blackness!=blackness && !t.deadness))
 
 	  // Customary diagonal capture, white
 	  ||(toY-y==-1 && !blackness && Math.abs(toX-x)==1 &&
-	     pieces.some((t)=>t.x==toX && t.y==toY && t.blackness!=blackness && !t.dead))
+	     pieces.some((t)=>t.x==toX && t.y==toY && t.blackness!=blackness && !t.deadness))
 
 	  //En passant
 	  ||enPassant(blackness,true,x,y,toX,toY).enpassant
@@ -167,46 +167,46 @@ const pawnCanMove = (blackness,x,y,toX,toY)=> {
 
     &&
 	   //No straight-on capture
-	   (!( pieces.some((t)=>t.x==toX && t.y==toY && t.blackness!=blackness && !t.dead) && toX==x	   ))
-	&& !pieces.some((t)=>t.x==toX && t.y==toY && t.blackness==blackness && !t.dead) //TODO refactor    
+	   (!( pieces.some((t)=>t.x==toX && t.y==toY && t.blackness!=blackness && !t.deadness) && toX==x	   ))
+	&& !pieces.some((t)=>t.x==toX && t.y==toY && t.blackness==blackness && !t.deadness) //TODO refactor    
 }
 
 const queenCanMove = (blackness,x,y,toX,toY)=>rookCanMove(blackness,x,y,toX,toY) || bishopCanMove(blackness,x,y,toX,toY)
 
 // The requirement to maintain N here sucks TODO
 const initPieces=()=>[
-    { sprite:Art.pawnb, x:0, y:1, n:0, canMove: pawnCanMove, blackness: true, kingness: false,  dead: false, pawnness: true },
-    { sprite:Art.pawnb, x:1, y:1, n:1, canMove: pawnCanMove, blackness: true, kingness: false,  dead: false, pawnness: true },
-    { sprite:Art.pawnb, x:2, y:1, n:2, canMove: pawnCanMove, blackness: true, kingness: false,  dead: false, pawnness: true },
-    { sprite:Art.pawnb, x:3, y:1, n:3, canMove: pawnCanMove, blackness: true, kingness: false,  dead: false, pawnness: true },
-    { sprite:Art.pawnb, x:4, y:1, n:4, canMove: pawnCanMove, blackness: true, kingness: false,  dead: false, pawnness: true },
-    { sprite:Art.pawnb, x:5, y:1, n:5, canMove: pawnCanMove, blackness: true, kingness: false,  dead: false, pawnness: true },
-    { sprite:Art.pawnb, x:6, y:1, n:6, canMove: pawnCanMove, blackness: true, kingness: false,  dead: false, pawnness: true },
-    { sprite:Art.pawnb, x:7, y:1, n:7, canMove: pawnCanMove, blackness: true, kingness: false,  dead: false, pawnness: true },
-    { sprite:Art.pawnw, x:0, y:6, n:8, canMove: pawnCanMove, blackness: false, kingness: false,  dead: false, pawnness: true },
-    { sprite:Art.pawnw, x:1, y:6, n:9, canMove: pawnCanMove, blackness: false, kingness: false,  dead: false, pawnness: true },
-    { sprite:Art.pawnw, x:2, y:6, n:10, canMove: pawnCanMove, blackness: false, kingness: false,  dead: false, pawnness: true },
-    { sprite:Art.pawnw, x:3, y:6, n:11, canMove: pawnCanMove, blackness: false, kingness: false,  dead: false, pawnness: true },
-    { sprite:Art.pawnw, x:4, y:6, n:12, canMove: pawnCanMove, blackness: false, kingness: false,  dead: false, pawnness: true },
-    { sprite:Art.pawnw, x:5, y:6, n:13, canMove: pawnCanMove, blackness: false, kingness: false,  dead: false, pawnness: true },
-    { sprite:Art.pawnw, x:6, y:6, n:14, canMove: pawnCanMove, blackness: false, kingness: false,  dead: false, pawnness: true },
-    { sprite:Art.pawnw, x:7, y:6, n:15, canMove: pawnCanMove, blackness: false, kingness: false,  dead: false, pawnness: true },
-    { sprite:Art.rookb, x:0, y:0,n:16, canMove: rookCanMove, blackness: true,  kingness: false, dead: false },
-    { sprite:Art.rookb, x:7, y:0,n:17, canMove: rookCanMove, blackness: true,  kingness: false, dead: false },
-    { sprite:Art.bishopb, x:2, y:0, n:18, canMove: bishopCanMove, blackness: true,  kingness: false, dead: false },
-    { sprite:Art.bishopb, x:5, y:0, n:19, canMove: bishopCanMove, blackness: true,  kingness: false, dead: false },
-    { sprite:Art.queenb, x:3, y:0, n:20, canMove: queenCanMove, blackness: true, kingness: false,  dead: false },
-    { sprite:Art.kingb, x:4, y:0, n:21, canMove: kingCanMove, blackness: true,  kingness: true,dead: false },
-    { sprite:Art.knightb, x:1, y:0, n:30, canMove: knightCanMove, blackness: true,  kingness: false, dead: false },
-    { sprite:Art.knightb, x:6, y:0, n:31, canMove: knightCanMove, blackness: true,  kingness: false, dead: false },
-    { sprite:Art.rookw, x:7, y:7, n:22, canMove: rookCanMove, blackness: false,  kingness: false, dead: false },
-    { sprite:Art.rookw, x:0, y:7, n:23, canMove: rookCanMove, blackness: false,  kingness: false, dead: false },
-    { sprite:Art.bishopw, x:2, y:7, n:24, canMove: bishopCanMove, blackness: false,  kingness: false, dead: false },
-    { sprite:Art.bishopw, x:5, y:7, n:25, canMove: bishopCanMove, blackness: false,  kingness: false, dead: false },
-    { sprite:Art.queenw, x:3, y:7, n:26, canMove: queenCanMove, blackness: false, kingness: false,  dead: false },
-    { sprite:Art.kingw, x:4, y:7, n:27, canMove: kingCanMove, blackness: false,  kingness: true,dead: false },
-    { sprite:Art.knightw, x:1, y:7, n:28, canMove: knightCanMove, blackness: false,  kingness: false, dead: false },
-    { sprite:Art.knightw, x:6, y:7, n:29, canMove: knightCanMove, blackness: false,  kingness: false, dead: false },
+    { sprite:Art.pawnb, x:0, y:1, n:0, canMove: pawnCanMove, blackness: true, kingness: false,  deadness: false, pawnness: true },
+    { sprite:Art.pawnb, x:1, y:1, n:1, canMove: pawnCanMove, blackness: true, kingness: false,  deadness: false, pawnness: true },
+    { sprite:Art.pawnb, x:2, y:1, n:2, canMove: pawnCanMove, blackness: true, kingness: false,  deadness: false, pawnness: true },
+    { sprite:Art.pawnb, x:3, y:1, n:3, canMove: pawnCanMove, blackness: true, kingness: false,  deadness: false, pawnness: true },
+    { sprite:Art.pawnb, x:4, y:1, n:4, canMove: pawnCanMove, blackness: true, kingness: false,  deadness: false, pawnness: true },
+    { sprite:Art.pawnb, x:5, y:1, n:5, canMove: pawnCanMove, blackness: true, kingness: false,  deadness: false, pawnness: true },
+    { sprite:Art.pawnb, x:6, y:1, n:6, canMove: pawnCanMove, blackness: true, kingness: false,  deadness: false, pawnness: true },
+    { sprite:Art.pawnb, x:7, y:1, n:7, canMove: pawnCanMove, blackness: true, kingness: false,  deadness: false, pawnness: true },
+    { sprite:Art.pawnw, x:0, y:6, n:8, canMove: pawnCanMove, blackness: false, kingness: false,  deadness: false, pawnness: true },
+    { sprite:Art.pawnw, x:1, y:6, n:9, canMove: pawnCanMove, blackness: false, kingness: false,  deadness: false, pawnness: true },
+    { sprite:Art.pawnw, x:2, y:6, n:10, canMove: pawnCanMove, blackness: false, kingness: false,  deadness: false, pawnness: true },
+    { sprite:Art.pawnw, x:3, y:6, n:11, canMove: pawnCanMove, blackness: false, kingness: false,  deadness: false, pawnness: true },
+    { sprite:Art.pawnw, x:4, y:6, n:12, canMove: pawnCanMove, blackness: false, kingness: false,  deadness: false, pawnness: true },
+    { sprite:Art.pawnw, x:5, y:6, n:13, canMove: pawnCanMove, blackness: false, kingness: false,  deadness: false, pawnness: true },
+    { sprite:Art.pawnw, x:6, y:6, n:14, canMove: pawnCanMove, blackness: false, kingness: false,  deadness: false, pawnness: true },
+    { sprite:Art.pawnw, x:7, y:6, n:15, canMove: pawnCanMove, blackness: false, kingness: false,  deadness: false, pawnness: true },
+    { sprite:Art.rookb, x:0, y:0,n:16, canMove: rookCanMove, blackness: true,  kingness: false, deadness: false },
+    { sprite:Art.rookb, x:7, y:0,n:17, canMove: rookCanMove, blackness: true,  kingness: false, deadness: false },
+    { sprite:Art.bishopb, x:2, y:0, n:18, canMove: bishopCanMove, blackness: true,  kingness: false, deadness: false },
+    { sprite:Art.bishopb, x:5, y:0, n:19, canMove: bishopCanMove, blackness: true,  kingness: false, deadness: false },
+    { sprite:Art.queenb, x:3, y:0, n:20, canMove: queenCanMove, blackness: true, kingness: false,  deadness: false },
+    { sprite:Art.kingb, x:4, y:0, n:21, canMove: kingCanMove, blackness: true,  kingness: true,deadness: false },
+    { sprite:Art.knightb, x:1, y:0, n:30, canMove: knightCanMove, blackness: true,  kingness: false, deadness: false },
+    { sprite:Art.knightb, x:6, y:0, n:31, canMove: knightCanMove, blackness: true,  kingness: false, deadness: false },
+    { sprite:Art.rookw, x:7, y:7, n:22, canMove: rookCanMove, blackness: false,  kingness: false, deadness: false },
+    { sprite:Art.rookw, x:0, y:7, n:23, canMove: rookCanMove, blackness: false,  kingness: false, deadness: false },
+    { sprite:Art.bishopw, x:2, y:7, n:24, canMove: bishopCanMove, blackness: false,  kingness: false, deadness: false },
+    { sprite:Art.bishopw, x:5, y:7, n:25, canMove: bishopCanMove, blackness: false,  kingness: false, deadness: false },
+    { sprite:Art.queenw, x:3, y:7, n:26, canMove: queenCanMove, blackness: false, kingness: false,  deadness: false },
+    { sprite:Art.kingw, x:4, y:7, n:27, canMove: kingCanMove, blackness: false,  kingness: true,deadness: false },
+    { sprite:Art.knightw, x:1, y:7, n:28, canMove: knightCanMove, blackness: false,  kingness: false, deadness: false },
+    { sprite:Art.knightw, x:6, y:7, n:29, canMove: knightCanMove, blackness: false,  kingness: false, deadness: false },
 
     
 ]
@@ -216,7 +216,7 @@ let pieces=initPieces();
 function GameInner(props){
     return(<>
 	{props.boardx.map((t)=>(
-	    <Piece n={t.n} key={t.n} dead={t.dead} x={t.x} y={t.y} sprite={t.sprite}
+	    <Piece n={t.n} key={t.n} deadness={t.deadness} x={t.x} y={t.y} sprite={t.sprite}
 	    canMove={props.canMove}  causesCheck={props.causesCheck} movePiece={props.movePiece}  dbgString={props.dbgString}
 	    moveCount={props.moveCount}  
 	    />))}
@@ -227,7 +227,7 @@ function GameInner(props){
 //Can be in App?
 function possibleMoves(blackness,causesCheck,max,setDbg,dbgString){
     let returnable = [];
-    pieces.filter((t)=>t.blackness==blackness && !t.dead).forEach((t)=> {
+    pieces.filter((t)=>t.blackness==blackness && !t.deadness).forEach((t)=> {
 	if(returnable.length<max){
 	    for(let i=0; i<8; ++i){
 		for(let j=0; j<8; ++j){
@@ -319,7 +319,7 @@ function Game(props){
 //Can be in App?
 function canMakeAMove(blackness,causesCheck){
     let returnable = false;
-    pieces.filter((t)=>t.blackness==blackness && !t.dead).forEach((t)=> {
+    pieces.filter((t)=>t.blackness==blackness && !t.deadness).forEach((t)=> {
 	for(let i=0; i<8; ++i){
 	    for(let j=0; j<8; ++j){
 		if(!returnable){
@@ -347,20 +347,20 @@ const App = ()=>{
 	const boardxn = boardx.filter(t=>t.n==n)[0]
 	
 	//Capture anything there 
-	const enemy=prime.filter((t)=>t.x==x && t.y==y && t.blackness != boardxn.blackness && !t.dead)
-	if(enemy.length==1) { enemy[0].dead=true; }
+	const enemy=prime.filter((t)=>t.x==x && t.y==y && t.blackness != boardxn.blackness && !t.deadness)
+	if(enemy.length==1) { enemy[0].deadness=true; }
 
 	//enpassant
 	const {enpassant,capturedX,capturedY} = enPassant(boardxn.blackness,boardxn.pawnness,boardxn.x,boardxn.y,x,y) 
-	if(enpassant) { prime.filter((t)=>t.x==capturedX && t.y==capturedY )[0].dead=true; }
+	if(enpassant) { prime.filter((t)=>t.x==capturedX && t.y==capturedY )[0].deadness=true; }
 	
 	let savex=boardxn.x; let savey=boardxn.y;
 	boardxn.x=x; boardxn.y=y;
 	let returnable = isChecked(boardxn.blackness/*, setDbg, dbgString*/);
 	boardxn.x=savex;
 	boardxn.y=savey;
-	if(enemy.length==1) { enemy[0].dead=false; }
-	if(enpassant) { prime.filter((t)=>t.x==capturedX && t.y==capturedY )[0].dead=false; }
+	if(enemy.length==1) { enemy[0].deadness=false; }
+	if(enpassant) { prime.filter((t)=>t.x==capturedX && t.y==capturedY )[0].deadness=false; }
 
 	setBoardx(prime)
 	return returnable
@@ -387,12 +387,12 @@ const App = ()=>{
 	let prime=[...boardx]
 	const boardxn = boardx.filter(t=>t.n==n)[0]
 	//Capture anything there
-	const enemy=prime.filter((t)=>t.x==x && t.y==y && t.blackness != boardxn.blackness && !t.dead)
-	if(enemy.length==1) { enemy[0].dead=true; }
+	const enemy=prime.filter((t)=>t.x==x && t.y==y && t.blackness != boardxn.blackness && !t.deadness)
+	if(enemy.length==1) { enemy[0].deadness=true; }
 
 	//enpassant
 	const {enpassant,capturedX,capturedY} = enPassant(boardxn.blackness,boardxn.pawnness,boardxn.x,boardxn.y,x,y) 
-	if(enpassant) { prime.filter((t)=>t.x==capturedX && t.y==capturedY )[0].dead=true; }
+	if(enpassant) { prime.filter((t)=>t.x==capturedX && t.y==capturedY )[0].deadness=true; }
 	
 	let savex=boardxn.x;
 	let savey=boardxn.y;
@@ -404,8 +404,8 @@ const App = ()=>{
 	    //Uh-oh! Revert illegal check-causing move.
 	    boardxn.x=savex;
 	    boardxn.y=savey;
-	    if(enemy.length==1) { enemy[0].dead=false; }
-	    if(enpassant) { prime.filter((t)=>t.x==capturedX && t.y==capturedY )[0].dead=false; }
+	    if(enemy.length==1) { enemy[0].deadness=false; }
+	    if(enpassant) { prime.filter((t)=>t.x==capturedX && t.y==capturedY )[0].deadness=false; }
 	    setBoardx(prime)
 	}else{
 	    // Mark piece dirty

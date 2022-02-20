024926e11934ec1eb99c8d7145c0d3166f242429
dell <beau.wilkinson@gmail.com>
2022-02-20 18:40:11 -0500

    Computer can use en passant

--- Engine.js
+++ Engine.js
@@ -1,5 +1,7 @@
-const Engine = {
+import Pawn from './Pawn';
 
+const Engine = {
+    // Returns possible moves for a color, sorted from best to worst.
     PossibleMoves: (blackness,causesCheck,max,pieces)=>{
 	let returnable = [];
 	pieces.filter((t)=>t.blackness==blackness && !t.deadness).forEach((t)=> {
@@ -11,15 +13,19 @@ const Engine = {
 			    if(cm){
 				let nocheck= !causesCheck(t.n,i,j);
 				if(nocheck) {
-				    //TODO this doesn't really encompass enpassant. Hurts performance in some mild way.
 				    let takenPiece=pieces.filter(u=>u.blackness!=blackness && !u.deadness && u.x==i && u.y==j)[0]?.value ?? 0;
-				    returnable.push({n:t.n, x:i, y:j, takenPiece:takenPiece});				    
+
+				    //Account for capture en passant 
+				    const {enpassant,capturedX,capturedY} =
+					  Pawn.EnPassant(t.blackness,t.pawnness,t.x,t.y,i,j,pieces);
+				    
+				    if(enpassant) { takenPiece=1; }				   
+				    returnable.push({n:t.n, x:i, y:j, takenPiece:takenPiece});
 				}
 			    }
 			}
 		    }}}});
 	returnable = returnable.sort((a,b) => b.takenPiece - a.takenPiece);
-//	alert(JSON.stringify(returnable));
 	return returnable;
     },
 };

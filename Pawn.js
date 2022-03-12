dd3fe1e20cf0d2ea48b75030818bc0577ad6b8d6
Beau <beau@Beaus-MBP.attlocal.net>
2022-03-12 18:21:35 -0500

    Vert. spacing cleanup

--- Pawn.js
+++ Pawn.js
@@ -1,48 +1,47 @@
 import Movement from './Movement';
 
-
 const Pawn = {
-
     EnPassant:  (blackness,pawnness,x,y,toX,toY,pieces)=> {
 	const happened = pawnness && (
 	    //Black
 	    (toY-y==1 && blackness && Math.abs(toX-x)==1 &&
 	     pieces.some((t)=>t.x==toX && t.justAdvancedTwo && t.y==y && t.pawnness  && t.blackness!=blackness && !t.deadness))||
-	    //White
+	        //White
 	    (toY-y==-1 && !blackness && Math.abs(toX-x)==1 &&
 	     pieces.some((t)=>t.x==toX && t.justAdvancedTwo && t.y==y && t.pawnness  && t.blackness!=blackness && !t.deadness)));
 	return {enpassant:happened, capturedX:toX, capturedY:y}
     },
     
     CanMove:  (blackness,x,y,toX,toY,pieces)=> {
-	return ((toY-y==1 && blackness) ||
+	return ((toY-y==1 && blackness) ||    //Start with the vertical movement rules...
 		(toY-y==-1 && !blackness) ||
 		(toY-y==2 && blackness && y==1  && Movement.NoInterveningPiece(x,y,toX,toY,pieces)) ||
 		(toY-y==-2 && !blackness && y==6  && Movement.NoInterveningPiece(x,y,toX,toY,pieces)) 	   
-	)
+	       )
+        
         // No jumping by pawns
 	    && Movement.NoInterveningPiece(x,y,toX,toY,pieces)
 
-   &&(toX==x
+        // Enforce the horizontal movement rules
+            &&(toX==x // Move straight ahead is OK
 
-		   //Capture (pawn)
-		   // Customary diagonal capture, black
-	      ||(toY-y==1 && blackness && Math.abs(toX-x)==1 &&
-		 pieces.some((t)=>t.x==toX && t.y==toY && t.blackness!=blackness && !t.deadness))
+	       //Capture (pawn)
+	       // Customary diagonal capture, black
+	       ||(toY-y==1 && blackness && Math.abs(toX-x)==1 &&
+		  pieces.some((t)=>t.x==toX && t.y==toY && t.blackness!=blackness && !t.deadness))
 
-		   // Customary diagonal capture, white
-	      ||(toY-y==-1 && !blackness && Math.abs(toX-x)==1 &&
-		 pieces.some((t)=>t.x==toX && t.y==toY && t.blackness!=blackness && !t.deadness))
+	       // Customary diagonal capture, white
+	       ||(toY-y==-1 && !blackness && Math.abs(toX-x)==1 &&
+		  pieces.some((t)=>t.x==toX && t.y==toY && t.blackness!=blackness && !t.deadness))
 
-		   //En passant
-	      ||Pawn.EnPassant(blackness,true,x,y,toX,toY,pieces).enpassant
+	       //En passant
+	       ||Pawn.EnPassant(blackness,true,x,y,toX,toY,pieces).enpassant
 
-	       )
+	      )
 
-
-    &&
-	       //No straight-on capture
-	       (!( pieces.some((t)=>t.x==toX && t.y==toY && t.blackness!=blackness && !t.deadness) && toX==x	   ))
+            &&
+	    //No straight-on capture
+	(!( pieces.some((t)=>t.x==toX && t.y==toY && t.blackness!=blackness && !t.deadness) && toX==x	   ))
 	    && !pieces.some((t)=>t.x==toX && t.y==toY && t.blackness==blackness && !t.deadness) //TODO refactor    
 
     },

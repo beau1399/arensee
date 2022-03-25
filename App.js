d1835e8787162a743de3ca451015d0933e816c5f
Beau <beau@Beaus-MBP.attlocal.net>
2022-03-24 21:54:06 -0400

    Minor code cleanup 1

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+// ARENSEE - React Native Chess
+//
 // If you have just cloned the repo, you need to run (from repo root "arensee"):
 //
 // npm install
@@ -13,6 +15,9 @@
 //
 //  echo sdk.dir=$ANDROID_SDK_ROOT > ./android/local.properties
 //
+// By default the user will play against the computer. To build for 2 players (or 0)
+//  you will need to edit Constants.js.
+//
 import React, {useState, useEffect, Component } from 'react';
 import {Game} from './Game';
 import Constants from './Constants';
@@ -20,16 +25,18 @@ import Movement from './Movement';
 import Pawn from './Pawn';
 import Queen from './Queen';
 
+// Outermost "App" component- the place to which state is "lifted up" and where check and mate are detected
 const App = ()=>{
     const [boardState, setBoardState] = useState(Constants.StartingBoard())
     const [moveCount, setMoveCount] = useState(0)
-    const [modalVisible,setModalVisible]=useState(undefined);
+    const [modalVisible, setModalVisible]=useState(undefined);
 
     const isChecked = (blackness)=> {
-        //King of the color that might be checked. We assume this exists.
+        //Find king of the color that might be checked.
         const k=boardState.filter((t,i)=>t.blackness==blackness && t.kingness && !t.deadness)[0];
         let returnable = false;
         if(k) {
+            // Look for a piece of the opposite color that can move where the king is.
             returnable=
                 boardState.filter((t)=>!t.deadness).some(
                     (t,i)=>t.blackness!=blackness && (t.x!=k.x || t.y!=k.y) && t.canMove(t.blackness,t.x,t.y,k.x,k.y,boardState));
@@ -37,28 +44,40 @@ const App = ()=>{
         return returnable;
     }
 
+    // Answers question "Does move of piece n to (x,y) cause check?". We can check for own-color check (opponent=false)
+    //  or other-color check (opponent=true). 
     const causesCheck = (n, x, y, opponent)=> {
+        //Construct next board state so we can simulate the move to gauge impact
         let prime=[...boardState]
-
-        const movingPiece = boardState.filter(t=>t.n==n)[0]
         
-        //Capture anything there 
+        //Find moving piece
+        const movingPiece = prime.filter(t=>t.n==n)[0]
+        
+        //Capture anything already where it's moving to 
         const enemy=prime.filter((t)=>t.x==x && t.y==y && t.blackness != movingPiece.blackness && !t.deadness)
         if(enemy.length==1) { enemy[0].deadness=true; }
 
-        //enpassant
-        const {enpassant,capturedX,capturedY} = Pawn.EnPassant(movingPiece.blackness,movingPiece.pawnness,movingPiece.x,movingPiece.y,x,y,boardState) 
+        //Check for and enforce capture en passant
+        const {enpassant,capturedX,capturedY} =
+              Pawn.EnPassant(movingPiece.blackness, movingPiece.pawnness, movingPiece.x, movingPiece.y, x, y, boardState) 
         if(enpassant) { prime.filter((t)=>t.x==capturedX && t.y==capturedY )[0].deadness=true; }
-        
-        let savex=movingPiece.x; let savey=movingPiece.y;
-        movingPiece.x=x; movingPiece.y=y;
+
+        //Move is performed hypothetically so we save state of piece n
+        let savex=movingPiece.x;
+        let savey=movingPiece.y;
+        movingPiece.x=x;
+        movingPiece.y=y;
+
+        //Get a verdict
         let returnable = isChecked(opponent ? !movingPiece.blackness : movingPiece.blackness);
+
+        //Restore state
         movingPiece.x=savex;
         movingPiece.y=savey;
         if(enemy.length==1) { enemy[0].deadness=false; }
         if(enpassant) { prime.filter((t)=>t.x==capturedX && t.y==capturedY )[0].deadness=false; }
-
         setBoardState(prime)
+        
         return returnable
     }
 
@@ -66,23 +85,36 @@ const App = ()=>{
     
     const causesEnemyCheck = (n, x, y)=> causesCheck(n,x,y,true)
 
+    // E.g. at game end
     const ResetBoard = ()=>{
         setBoardState(Constants.StartingBoard())
         setMoveCount(0)
     }
-    
+
+    // Piece move handler
+    //
+    // Beyond mutating board state, this is also the locus of some bookkeeping code (marking dirty,
+    //  notating two-square pawn moves for subsequnet en passant) and the place where illegal
+    //  self-checking moves by human players are rejected (in which case param "prechecked" is false...
+    //  the computer's chess engine will already have filtered out any self-checking moves as part of
+    //  its analysis).
     const movePiece = (n, x, y, prechecked)=> {
 
+        //Construct new board state
         let prime=[...boardState]
-        const movingPiece = boardState.filter(t=>t.n==n)[0]
+
+        //Get piece n, the moving piece
+        const movingPiece = prime.filter(t=>t.n==n)[0]
+
         //Capture anything there
         const enemy=prime.filter((t)=>t.x==x && t.y==y && t.blackness != movingPiece.blackness && !t.deadness)
         if(enemy.length==1) { enemy[0].deadness=true; }
 
-        //enpassant
+        //Detect and enforce enpassant
         const {enpassant,capturedX,capturedY} = Pawn.EnPassant(movingPiece.blackness,movingPiece.pawnness,movingPiece.x,movingPiece.y,x,y,boardState) 
         if(enpassant) { prime.filter((t)=>t.x==capturedX && t.y==capturedY )[0].deadness=true; }
-        
+
+        //Save state in case the (human's) move attempt is illegal
         let savex=movingPiece.x;
         let savey=movingPiece.y;
         movingPiece.x=x;

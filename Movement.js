aad7aef5c180688788a1e66476f745c6ed36f11d
Beau <beau@Beaus-MBP.lan>
2022-07-04 12:21:51 -0400

    Use named constants vs. hardcoding

--- Movement.js
+++ Movement.js
@@ -56,8 +56,8 @@ const Movement = {
     
     Release: (e, t) => {
         const pieces = t.props.board;
-        const targetX = (Math.floor((e.nativeEvent.pageX-9) / Constants.SquareSize));
-        const targetY = (Math.floor((e.nativeEvent.pageY-138) / Constants.SquareSize));
+        const targetX = (Math.floor((e.nativeEvent.pageX-Constants.SpriteWidth) / Constants.SquareSize));
+        const targetY = (Math.floor((e.nativeEvent.pageY-Constants.BoardTop) / Constants.SquareSize));
         if (targetX>7 || targetY>7 || targetX<0 || targetY<0) { return; }
         const n=t.props.n;
         const piecesn=pieces.filter((u)=>u.n==n)[0];

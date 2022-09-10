86f586bb9e9cbd67c3cca030743d38d552106fc8
Beau <beau@Beaus-MBP.attlocal.net>
2022-09-09 21:40:47 -0400

    Clean up whitspace etc.

--- Sprite.js
+++ Sprite.js
@@ -8,21 +8,24 @@ import { View } from 'react-native';
 //  that gives context to the appearance string and allows it to be rendered.
 //
 /*
-          <Sprite pixelSize={24}
-                  sprite={["x.",".x"]}
-                  letterToColor={{"x":"yellow", ".":"brown"}} />
+  <Sprite pixelSize={24}
+  sprite={["x.",".x"]}
+  letterToColor={{"x":"yellow", ".":"brown"}} />
 */
 //
 function Sprite(props) {
     const markup = (<View style={{flexDirection: "column"}}>				    
-	    {props.sprite.map((t,i)=>{
-		return (<View key={"sprite-outer-" + i + props.x + props.y} style={{width:1, height:props.pixelSize, flexDirection: "row"}}>
-		    {t.split('').map((u,j)=>{
-			return (<View key={"sprite-inner-" + (i * 100 + j) + props.x + props.y} style={{width:props.pixelSize, height:props.pixelSize,
-							     backgroundColor:props.letterToColor[u]}} />);})}
-		    </View>);})}
-	    </View>
-	);
+	                {props.sprite.map((t,i)=>{
+                            // Some magic strings in the next few lines just to ensure unique IDs... sorry :(
+		            return (<View key={"sprite-outer-" + i + props.x + props.y}
+                                          style={{width:1, height:props.pixelSize, flexDirection: "row"}}>
+		                        {t.split('').map((u,j)=>{
+			                    return
+                                            (<View key={"sprite-inner-" + (i * 100 + j) + props.x + props.y}
+                                                   style={{width:props.pixelSize, height:props.pixelSize, backgroundColor:props.letterToColor[u]}} />);})}
+		                    </View>);})}
+	            </View>
+	           );
     return <View>{markup}</View>
 }
 
